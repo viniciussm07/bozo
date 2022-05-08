6d07d50b5f7dd69b366d226e357969b554d97de3
viniciussm07 <vini.soares.martins@gmail.com>
2022-05-08 10:16:51 -0300

    first commit (project already working)

---
+++ src/Bozo.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+public class Bozo {
+    public static void main(String[] args) throws Exception {
+        int contadorRodada = 1;
+        Placar placar = new Placar();
+        RolaDados dados = new RolaDados(5);
+        String bufferString = "";
+        int bufferInt;
+        ArrayList<Integer> arrListInt = new ArrayList<>();
+        boolean[] quais = { false, false, false, false, false };
+        while (contadorRodada < 11) {
+            do {
+                System.out.println("****** Rodada: " + contadorRodada + "\nPrecione ENTER para iniciar a rodada");
+                bufferString = EntradaTeclado.leString();
+            } while (bufferString.length() != 0);
+            dados.rolar();
+            System.out.println(dados.toString());
+            for (int j = 0; j < 2; j++) {
+                System.out.println("Digite o número do dado que quiser rolar novamente (separado por espaços), caso não queira apenas precione Enter");
+
+                bufferString = EntradaTeclado.leString();
+                if (bufferString != "") {
+                    for (int i = 0; i < bufferString.length(); i++) {
+                        if (bufferString.charAt(i) != ' ' && bufferString.charAt(i) < '6'
+                                && bufferString.charAt(i) > '0') {
+                            arrListInt.add(Character.getNumericValue(bufferString.charAt(i)));
+                        }
+                    }
+                    for (int i = 0; i < arrListInt.size(); i++) {
+                        quais[arrListInt.get(i) - 1] = true;
+                    }
+                    dados.rolar(quais);
+                    System.out.println(dados.toString());
+                    
+                }
+                for (int k = 0; k < quais.length; k++) {
+                    quais[k] = false;
+                }
+                arrListInt.clear();
+            }
+
+            try {
+                System.out.println(placar.toString());
+                System.out.println("Digite uma posição para atualizar seu placar");
+                bufferInt = EntradaTeclado.leInt();
+                System.out.println("\n");
+            } catch (Exception e) {
+                System.out.println("Não foi digitado um inteiro");
+                return;
+            }
+            placar.add(bufferInt, dados.valoresDados);
+            System.out.println(placar.toString());
+
+            contadorRodada++;
+            // Resetando variáveis
+            bufferString = "";
+            bufferInt = 0;
+            for (int i = 0; i < quais.length; i++) {
+                quais[i] = false;
+            }
+            arrListInt.clear();
+        }
+        /*
+         * Pedir para o usuário escolher os dados a serem rolados
+         * Rolar os dados que o usuário selecionou (2a vez)
+         * Pedir para o usuário escolher os dados a serem rolados
+         * Rolar os dados que o usuário selecionou (3a vez)
+         * Mostrar o placar atual
+         * Pedir para o usuário selecionar a posição que quer ocupar
+         * Mostrar o placar atualizado
+         * No final, mostrar o número de pontos obtidos
+         */
+        System.out.println("Sua pontuação foi de: " + placar.getScore());
+    }
+}
---
+++ src/RolaDados.java
@@ -0,0 +1,64 @@
+public class RolaDados {
+    Dado[] dados ;
+    int[] valoresDados;
+
+    public RolaDados(int n){
+        dados = new Dado[n];
+        valoresDados = new int[n];
+    }
+
+    public int[] rolar(){
+        for (int i = 0; i < dados.length; i++){
+            dados[i] = new Dado();
+            valoresDados[i] = dados[i].rolar();
+            try {
+                Thread.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return valoresDados;
+    }
+
+    public int[] rolar(boolean[] quais){
+        for (int i = 0; i < quais.length; i++){
+            if (quais[i] == true){
+                valoresDados[i] = dados[i].rolar();
+                try {
+                    Thread.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return valoresDados;
+    }
+
+    private String[][] faces = { { "+-----+    ", "|     |    ", "|  *  |    ", "|     |    ", "+-----+    " },
+            { "+-----+    ", "|*    |    ", "|     |    ", "|    *|    ", "+-----+    " },
+            { "+-----+    ", "|*    |    ", "|  *  |    ", "|    *|    ", "+-----+    " },
+            { "+-----+    ", "|*   *|    ", "|     |    ", "|*   *|    ", "+-----+    " },
+            { "+-----+    ", "|*   *|    ", "|  *  |    ", "|*   *|    ", "+-----+    " },
+            { "+-----+    ", "|*   *|    ", "|*   *|    ", "|*   *|    ", "+-----+    " } };
+
+    @Override
+    // Só funciona para 5 dados
+    public String toString(){
+        String s = " 1          2          3          4          5\n";
+
+        for (int j = 0; j < 5; j++){
+            for (int i = 0; i < 5; i++){
+                s += faces[valoresDados[i] - 1][j];
+            }
+            s += "\n";
+        }       
+        
+        return s;
+    }
+
+    public static void main(String[] args) {
+        RolaDados dados = new RolaDados(5);
+        dados.rolar();
+        System.out.printf(dados.toString());
+    }
+}
